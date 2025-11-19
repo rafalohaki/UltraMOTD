e8689b545b4d0b865bd32b336ee302f04798a2ca
rafal <rafalohaki@gmail.com>
2025-11-19 20:17:10 +0100

    essa

---
+++ src/main/java/org/rafalohaki/ultraMOTD/UltraMOTD.java
@@ -0,0 +1,18 @@
+package org.rafalohaki.ultraMOTD;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.plugin.Plugin;
+import org.slf4j.Logger;
+
+@Plugin(id = "ultramotd", name = "UltraMOTD", version = "1.0-SNAPSHOT")
+public class UltraMOTD {
+
+    @Inject
+    private Logger logger;
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+    }
+}
